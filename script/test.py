a882d46c2633ceba1843da6ae3744d307960c8fb
sv223229 <nhatnguyen122001@gmail.com>
2026-01-05 23:08:02 -0700

    Update test.py

--- script/test.py
+++ script/test.py
@@ -1,6 +1,29 @@
 import requests
 
-x = requests.get('https://w3schools.com/python/demopage.htm')
+def get_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
 
+    try:
+        response = requests.get(url)
 
-print(x.text)
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            print('Error:', response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
+
+def main():
+    posts = get_posts()
+
+    if posts:
+        print('First Post Title:', posts[0]['title'])
+        print('First Post Body:', posts[0]['body'])
+    else:
+        print('Failed to fetch posts from API.')
+
+if __name__ == '__main__':
+    main()
